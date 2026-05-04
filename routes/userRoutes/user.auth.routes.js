d2a534b6dd54d49f04d2fee55ce823e7a4b38192
yakir <yakir2062@gmail.com>
2026-05-04 13:40:06 +0300

    added  id & verifyToken to  updatePassword

--- routes/userRoutes/user.auth.routes.js
+++ routes/userRoutes/user.auth.routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import { logUser,registerUser,resetUserPassword,updateUserPassword } from "../../controllers/user.auth.controller.js";
-
+import{verifyToken} from "../../utils/token.js"
 const router = Router();
 
 
 //POST
 router.post('/login',logUser)
 router.post('/register',registerUser)
-router.post("resetPassword",resetUserPassword) 
+router.post("/resetPassword",resetUserPassword) 
 
 //PATCH
-router.patch('updatePassword',updateUserPassword)
+router.patch('/:id/updatePassword',verifyToken,updateUserPassword)
 
 
 
